d4559d7243ab82341029a6e06f2d83cfc2bea7a9
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-11-27 19:20:13 +0300

    Add files via upload

--- math.test.js
+++ math.test.js
@@ -1,8 +1,18 @@
-import {test, it, expect} from 'vitest';
+import {it, expect} from 'vitest';
 import {add} from '../math'
 
 it('Should summarize all number values in an array', () => {
-    const result =  add([1, 2, 3]);
-    expect(result).toBe(6);
+    // Using The AAA Patern
 
+    // Arrange
+    const numbers = [1, 2, 3];
+    const expectedResult = numbers.reduce((previousValue, nextValue) => {
+        return previousValue + nextValue
+    }, 0)
+
+    // Act
+    const result =  add(numbers);
+
+    // Assert
+    expect(result).toBe(expectedResult);
 });
