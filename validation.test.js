8a5f475355e096536a479328bce07dda935f5903
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-11-30 19:23:31 +0300

    Add files via upload
    Test Suites

--- validation.test.js
+++ validation.test.js
@@ -1,20 +1,62 @@
-import {it, expect} from 'vitest';
+import {it, expect, describe} from 'vitest';
 import {validateStringNotEmpty, validateNumber} from './validation'
 
+// Test Suites
+describe('validateNumber()', () => {
 
-it('Should yield an error if an empty string is provided', () => {
-    const input = '';
+    it('Should yield a type of input', () => {
+        const input = 1;
+    
+        const validattionFn = validateNumber(input);
+    
+        expect(validattionFn).toBeTypeOf("number");
+    })
+    
+    
+    it('Should yield an error if non-numeric values is provided', () => {
+        const input = '1';
+    
+        const validattionFn = () => validateNumber(input);
+    
+        expect(validattionFn).not.toThrow();
+    })
 
-    const validattionFn = () =>  validateStringNotEmpty(input);
+});
 
-    expect(validattionFn).toThrow();
+describe('validateStringNotEmpty()', () => {
+
+    it('Should yield an error if an empty string is provided', () => {
+        const input = '';
+    
+        const validattionFn = () =>  validateStringNotEmpty(input);
+    
+        expect(validattionFn).toThrow();
+    })
+    
 })
 
+// it('Should yield an error if an empty string is provided', () => {
+//     const input = '';
 
-it('Should yield an error if an empty string is provided', () => {
-    const input = 1;
+//     const validattionFn = () =>  validateStringNotEmpty(input);
 
-    const validattionFn = validateNumber(input);
+//     expect(validattionFn).toThrow();
+// })
 
-    expect(validattionFn).toBeTypeOf("number");
-})
+
+// it('Should yield a type of input', () => {
+//     const input = 1;
+
+//     const validattionFn = validateNumber(input);
+
+//     expect(validattionFn).toBeTypeOf("number");
+// })
+
+
+// it('Should yield an error if non-numeric values is provided', () => {
+//     const input = '1';
+
+//     const validattionFn = () => validateNumber(input);
+
+//     expect(validattionFn).not.toThrow();
+// })
